1a4d170044b01bed8b326ff216be917473254ffa
Yasir Ali Mohamed <yaazir143@gmail.com>
2022-04-22 23:13:54 +0300

    first push

---
+++ javascript/animationEffects.js
@@ -0,0 +1,26 @@
+
+let oneText= document.getElementById('one').textContent
+let twoText= document.getElementById('two').textContent
+let threeText= document.getElementById('three').textContent
+let fourText= document.getElementById('four').textContent
+let fiveText= document.getElementById('five').textContent
+
+// function compareToWin(){
+//     // let win=oneText + twoText
+//     if((oneText===twoText)=true){
+//     }
+//     else{
+//         alert("your are the winner")
+
+//     }
+// }
+
+function changeIconText(){
+    let clickedto=document.getElementById().onclick
+    let iconButton =document.getElementById()
+    iconButton.textContent='p'
+}
+
+function winning(){
+
+}
