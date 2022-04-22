98377eef55d9e92cec68ff46102958315aa4523b
Yasir Ali Mohamed <yaazir143@gmail.com>
2022-04-23 01:53:28 +0300

    added comparison feature

--- javascript/animationEffects.js
+++ javascript/animationEffects.js
@@ -5,21 +5,32 @@ let threeText= document.getElementById('three').textContent
 let fourText= document.getElementById('four').textContent
 let fiveText= document.getElementById('five').textContent
 
-// function compareToWin(){
-//     // let win=oneText + twoText
-//     if((oneText===twoText)=true){
-//     }
-//     else{
-//         alert("your are the winner")
 
-//     }
-// }
+function compareToWin(){
+    // let win=oneText + twoText
+    if(oneText===twoText===fourText){
+        alert("your are the winner")
+    }
+    else{
+        alert("you lose")
+    }
+}
 
 function changeIconText(){
-    let clickedto=document.getElementById().onclick
-    let iconButton =document.getElementById()
+    let clickedto=document.getElementsByTagName("p")
     iconButton.textContent='p'
 }
+
+
+// on click selection
+// $(document).click(function () {
+//     alert($(this).text());
+// });
+
+
+// $(document).click(function(click) {
+//     alert($(event.target).text());
+// });
 
 function winning(){
 
